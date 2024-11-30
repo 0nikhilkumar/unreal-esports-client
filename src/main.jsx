b95abb6e8fb81f2ec16949b52105fbdc470489d1
arjunsaxena122 <arjunsaxena122@gmail.com>
2024-11-30 20:15:01 +0530

    Adding new code

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,16 @@ import { StrictMode } from "react";
 import App from '@/App'
 import Home from '@/Pages/Home/Home'
 import Error from '@/components/Error/Error'
-import About from '@/Pages/About/About'
+import About from './Pages/About/About'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import Tournament from "./Pages/Tournament/Tournament";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App/>} errorElement={<Error/>}>
-      <Route path="" element={<Home/>}/>
-      <Route path="about" element={<About/>}/>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/tournament" element={<Tournament/>}/>
     </Route>
   )
 )
