0c72f4ffdb019cbf27cc7b88b19ed267a09fc40f
arjunsaxena122 <arjunsaxena122@gmail.com>
2024-11-30 18:33:23 +0530

    Correct the code In Main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,22 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { StrictMode } from "react";
 import App from '@/App'
+import Home from '@/Pages/Home/Home'
+import Error from '@/components/Error/Error'
+import About from '@/Pages/About/About'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App/>} errorElement={<Error/>}>
+      <Route path="" element={<Home/>}/>
+      <Route path="about" element={<About/>}/>
+    </Route>
+  )
+)
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App/>
+    <RouterProvider router={router}/>
   </StrictMode>
 );
