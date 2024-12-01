48e221197cca72834655a23f6d2ef5b0bfe957fa
arjunsaxena122 <arjunsaxena122@gmail.com>
2024-12-01 16:26:30 +0530

    Add Footer Design Code

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,37 +6,56 @@ import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <footer className="footer footer-center bg-base-200 text-base-content rounded p-10">
-      <nav className="grid grid-flow-col gap-4">
-        <Link to={"/about"} className="link link-hover">
-          About us
-        </Link>
-        <Link to={"/tournament"} className="link link-hover">
-          Tournament
-        </Link>
-        <Link to={"/faq"} className="link link-hover">
-          FAQ
-        </Link>
-      </nav>
-      <nav>
-        <div className="grid grid-flow-col gap-4">
-          <a>
-            <FaTwitter className="text-xl" />
-          </a>
-          <a>
-            <FaYoutube className="text-xl" />
-          </a>
-          <a>
-            <FaFacebook className="text-xl" />
-          </a>
-          <a>
-            <FaInstagram className="text-xl" />
-          </a>
+    <footer className="bg-[#0A1623] text-white py-6">
+      <div className="max-w-7xl mx-auto px-4 flex flex-col md:flex-row justify-between items-center ">
+        <div>
+          {/* Logo Section */}
+          <div className="flex items-center space-x-3 mb-5">
+            <img
+              src="/images/logo.jpg"
+              alt="logo"
+              className="w-15 h-12 md:w-20 md:h-15 lg:w-24 lg:h-18 rounded-e-full"
+            />
+          </div>
+
+          {/* Copyright Section */}
+
+          <div className="text-white hover:text-gray-200 text-sm">
+            &copy; 2024 UnReal Esports. All rights reserved
+          </div>
         </div>
-      </nav>
-      <aside>
-        <p>© {new Date().getFullYear()} - All right reserved by Unreal Esports</p>
-      </aside>
+
+        <div>
+          {/* Links Section */}
+          <div className="flex space-x-6 mt-4 md:mt-0  mb-8">
+            <Link to={"/about"} className="text-white hover:text-gray-200">
+              About
+            </Link>
+            <Link to={"/tournament"} className="text-white hover:text-gray-200">
+              Tournament
+            </Link>
+            <Link to={"/faq"} className="text-white hover:text-gray-200">
+              FAQ
+            </Link>
+          </div>
+
+          {/* Social Media Icons */}
+          <div className="flex space-x-4 mt-4 md:mt-0">
+            <Link className="bg-gray-800 hover:bg-gray-600 text-white w-10 h-10 flex items-center justify-center rounded-full transition duration-300 text-xl">
+              <FaFacebook />
+            </Link>
+            <Link className="bg-gray-800 hover:bg-gray-600 text-white w-10 h-10 flex items-center justify-center rounded-full transition duration-300 text-xl">
+              <FaInstagram />
+            </Link>
+            <Link className="bg-gray-800 hover:bg-gray-600 text-white w-10 h-10 flex items-center justify-center rounded-full transition duration-300 text-xl">
+              <FaYoutube />
+            </Link>
+            <Link className="bg-gray-800 hover:bg-gray-600 text-white w-10 h-10 flex items-center justify-center rounded-full transition duration-300 text-xl">
+              <FaTwitter />
+            </Link>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 }
